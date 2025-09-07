e339fca0704b5f140b87d5b4e18bb0392598551d
2323212a <brayan.hernandez20@unach.mx>
2025-09-06 19:48:32 -0600

    confirmar

--- 3NLIDTS-BRAYAN-HERNANDEZ-04/Form1.cs
+++ 3NLIDTS-BRAYAN-HERNANDEZ-04/Form1.cs
@@ -2,11 +2,17 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TextBox;
+using System.IO;         
+using System.Diagnostics;
+
 
 namespace _3NLIDTS_BRAYAN_HERNANDEZ_04
 {
@@ -15,7 +21,75 @@ namespace _3NLIDTS_BRAYAN_HERNANDEZ_04
         public Form1()
         {
             InitializeComponent();
+
+            txtedad.TextChanged += ValidarEdad;
+            txtnombre.TextChanged += ValidarNombre;
+            txtapellido.TextChanged += ValidarApellido;
+            txttelefono.Leave += ValidarTelefono;
+            txtestatura.TextChanged += ValidarEstatura;
+
         }
+
+        private void ValidarNombre(object sender, EventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+            if (!EsTextoValido(textBox.Text))
+            {
+                MessageBox.Show("Por favor ingrese un nombre válido (solo letras y espacios).",
+                                "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                textBox.Clear();
+            }
+        }
+
+        private void ValidarApellido(object sender, EventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+            if (!EsTextoValido(textBox.Text))
+            {
+                MessageBox.Show("Por favor ingrese un apellido válido (solo letras y espacios).",
+                                "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                textBox.Clear();
+            }
+        }
+
+        private void ValidarEdad(object sender, EventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+            if (!Regex.IsMatch(textBox.Text, @"^[0-9]{1,2}$")) // Edad entre 0 y 99
+            {
+                MessageBox.Show("Ingrese una edad válida (solo números, máximo 2 dígitos).",
+                                "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                textBox.Clear();
+            }
+        }
+
+        private void ValidarEstatura(object sender, EventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+            if (!double.TryParse(textBox.Text, out _))
+            {
+                MessageBox.Show("Ingrese solo números en la estatura (ejemplo: 1.75).",
+                                "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                textBox.Clear();
+            }
+        }
+
+        private void ValidarTelefono(object sender, EventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+            if (!Regex.IsMatch(textBox.Text, @"^[0-9]{10}$")) // 10 dígitos
+            {
+                MessageBox.Show("Ingrese un teléfono válido de 10 dígitos.",
+                                "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                textBox.Clear();
+            }
+        }
+
+        private bool EsTextoValido(string valor)
+        {
+            return Regex.IsMatch(valor, @"^[a-zA-Z\s]+$");
+        }
+
 
         private void button1_Click(object sender, EventArgs e)
         {
@@ -35,23 +109,16 @@ namespace _3NLIDTS_BRAYAN_HERNANDEZ_04
             }
             string datos = $"Nombre: {nombre}\r\nApellido: {apellido}\r\nEdad: {edad}\r\nTeléfono: {telefono}\r\nEstatura: {estatura}\r\nGénero: {genero}";
             MessageBox.Show(datos, "Datos Guardados", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            string ruta = "datos.txt"; // El archivo se creará en la carpeta del programa
+            File.AppendAllText(ruta, datos + Environment.NewLine + "---------------------" + Environment.NewLine);
         }
-        private void btnBorar_Click(object sender, EventArgs e)
+        private void btnregistros_Click_1(object sender, EventArgs e)
         {
-            txtnombre.Clear();
-            txtapellido.Clear();
-            txtedad.Clear();
-            txttelefono.Clear();
-            txtestatura.Clear();
-            rbfemenino.Checked = false;
-            rbmasculino.Checked = false;
-        }
 
             string ruta = "datos.txt";
 
             if (File.Exists(ruta))
             {
-                // Abrir el archivo en el bloc de notas
                 Process.Start("notepad.exe", ruta);
             }
             else
@@ -61,6 +128,22 @@ namespace _3NLIDTS_BRAYAN_HERNANDEZ_04
                                 MessageBoxButtons.OK,
                                 MessageBoxIcon.Information);
             }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btnborrar_Click(object sender, EventArgs e)
+        {
+            txtnombre.Clear();
+            txtapellido.Clear();
+            txtedad.Clear();
+            txttelefono.Clear();
+            txtestatura.Clear();
+            rbfemenino.Checked = false;
+            rbmasculino.Checked = false;
         }
     }
 }
